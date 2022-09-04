244583782a4bcfef7c548cc085c3b4e12e52e355
Guilherme David <guilhermedavidrk@icloud.com>
2022-09-04 18:13:07 -0300

    :art: add styles global header/ NavLink/ icons/ logo...

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { ThemeProvider } from 'styled-components'
-// import { Button } from './components/Button'
 import { BrowserRouter } from 'react-router-dom'
 import { Router } from './Router'
 import { GlobalStyle } from './styles/global'
--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -1,3 +1,7 @@
 export function History() {
-  return <h1>hystory</h1>
+  return (
+    <div>
+      <h1>History</h1>
+    </div>
+  )
 }
