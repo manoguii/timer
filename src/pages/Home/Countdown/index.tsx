22d8564b9118d4e74fd67e046a2f5f3102726172
Guilherme David <guilhermedavidrk@icloud.com>
2022-09-10 22:08:23 -0300

    :pushpin: Finished project

--- src/pages/Home/Countdown/index.tsx
+++ src/pages/Home/Countdown/index.tsx
@@ -21,7 +21,7 @@ export function Countdown() {
       interval = setInterval(() => {
         const secondsDiference = differenceInSeconds(
           new Date(),
-          activeCycle.startDate,
+          new Date(activeCycle.startDate),
         )
 
         if (secondsDiference >= totalSeconds) {
